6679b9608de52c6749bee1ff500415e3d5fce9af
zuykee <64494112+zuykee@users.noreply.github.com>
2021-08-20 09:32:24 +0300

    feat: finish the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+      let str = String(Math.abs(n));
+     let strev = new String;
+     
+    
+     for (let i = str.length - 1; i>=0; --i) {
+         strev = strev + str[i];
+    }
+    return Number(strev);
 }
